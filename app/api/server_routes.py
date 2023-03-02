9d7ed08daca1b75cc92a73194e7f84b663f2a5cf
Dan Kimball <dankimball@Dans-MacBook-Pro.local>
2023-03-01 18:05:43 -0700

    added second route

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -4,6 +4,7 @@ from app.models import Server, db
 
 server_routes = Blueprint('server', __name__)
 
+##get all servers 
 @server_routes.route('/')
 # @login_required
 def getAllServers():
@@ -14,12 +15,17 @@ def getAllServers():
     return jsonify({'servers': [server.to_dict() for server in servers]})
 
 
-
+##Get server by id
 @server_routes.route('/<int:id>')
 # @login_required
-def user(id):
+def serverById(id):
     """
     Query for a server by id and returns that server in a dictionary
     """
     server = Server.query.get(id)
     return server.to_dict()
+
+#get all servers of current user
+@server_routes.route('/current')
+def getServersOfCurrentUser():
+    pass
