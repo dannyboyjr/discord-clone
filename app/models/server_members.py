08014df67f5b0cb5dd44704224cc98a3d48ee2fb
scottkonner <scott_konner@yahoo.com>
2023-03-04 09:02:53 -0500

    server_members model touch up

--- app/models/server_members.py
+++ app/models/server_members.py
@@ -18,6 +18,9 @@ class Server_Member(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     server_id = db.Column(db.Integer, db.ForeignKey('servers.id'), nullable=False)
 
+    server = db.relationship('Server', back_populates='server_members')
+    user = db.relationship('User', back_populates='sever_members')
+
     def to_dict(self):
         return {
             'id':self.id,
