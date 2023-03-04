69c813b942ac365bdb60998f34a3aa9d9ebbc546
Dan Kimball <dankimball@Dans-MacBook-Pro.local>
2023-03-04 16:16:32 -0700

    built out routes for messages in channels for testing purposes (may be replaced by websockets.. maybe)

--- app/api/messages_routes.py
+++ app/api/messages_routes.py
@@ -1,48 +1,83 @@
 from flask import Blueprint, jsonify, request, redirect, url_for
 from flask_login import login_required, current_user
-from app.models import Message, db, Channel
+from app.models import Message, Server, Channel, Server_Member, db
 
 
 message_routes = Blueprint("messages", __name__)
 
 
-# @message_routes.route("/<int:id>", methods=["GET"])
-# @login_required
-# # def messages():
-# #     """
-# #     """
-
-
-@message_routes.route("/channels/<int:channel_id>/messages", methods=["GET"])
+@message_routes.route("<server_id>/channels/<int:channel_id>/messages", methods=["GET"])
 @login_required
-def get_message_channel_id(channel_id):
+def get_message_channel_id(server_id, channel_id):
     """
-    Gets a private message(DM) by channel id.
+    Get all messages of public channel.
 
     """
-
-    channel = Channel.query.get(channel_id)
-
+    server = Server.query.get(server_id)
+    if server is None:
+        return jsonify({"error": "Server not found"}), 404
+    if server.private is True:
+        return jsonify({"error": "cannot get private server"}), 403
+    
+    # Check if channel exists
+    channel = Channel.query.filter_by(server_id=server_id, id=channel_id).first()
     if channel is None:
         return jsonify({"error": "Channel not found"}), 404
 
-    messages = Message.query.filter_by(channel_id=channel_id).all()
+    # Check if channel is private
+    if channel.private is True:
+        return jsonify({"error": "Cannot send message to private channel"}), 403
 
+    #validate member
+    server_member = Server_Member.query.filter_by(server_id=server_id, user_id=current_user.id).first()
+    if not server_member:
+        return jsonify({"error": "You are not a member of this server."}), 403
+
+
+    messages = Message.query.filter_by(server_id=server_id, channel_id=channel_id).all()
     return {"Messages": [message.to_dict() for message in messages]}
 
 
-@message_routes.route("/channels/<int:channel_id>/messages", methods=["POST"])
+@message_routes.route("<int:server_id>/channels/<int:channel_id>/messages", methods=["POST"])
 @login_required
-def create_message_channel_id(channel_id):
+def create_message_channel_id(server_id,channel_id):
     """
-    Post message(DM) based on channel id
-
+    send message to channel
     """
-    channel = Channel.query.get(channel_id)
-
+    server = Server.query.get(server_id)
+    if server is None:
+        return jsonify({"error": "Server not found"}), 404
+    if server.private is True:
+        return jsonify({"error": "cannot get private server"}), 403
+    
+    # Check if channel exists
+    channel = Channel.query.filter_by(server_id=server_id, id=channel_id).first()
     if channel is None:
         return jsonify({"error": "Channel not found"}), 404
 
-    message = Message.query.filter_by(channel_id=channel_id).all()
+    # Check if channel is private
+    if channel.private is True:
+        return jsonify({"error": "Cannot send message to private channel"}), 403
 
-    return {"Message": [content.to_dict() for content in message]}
+    #check if member of server
+    server_member = Server_Member.query.filter_by(server_id=server_id, user_id=current_user.id).first()
+    if not server_member:
+        return jsonify({"error": "You are not a member of this server."}), 403
+
+    #json can only contain {"conent": "message"}
+    data = request.get_json()
+    if set(data.keys()) != {'content'}:
+        return jsonify({"error": "Invalid request data. Must only contain 'content' key."}), 400
+
+    message = Message(
+        content=data["content"],
+        owner_id=current_user.id,
+        channel_id=channel_id,
+        server_id=server_id
+    )
+
+    db.session.add(message)
+    db.session.commit()
+
+    return jsonify(message.to_dict()), 201
+
