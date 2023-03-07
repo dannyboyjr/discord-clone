9a5405d4df80130b331ce8d4c20990cf2f0ed4a4
Dan Kimball <dankimball@Dans-MacBook-Pro.local>
2023-03-06 19:45:56 -0700

    added get all servers Redux

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
+import serversReducer from './servers';
 
 const rootReducer = combineReducers({
   session,
+  servers:serversReducer,
 });
 
 
