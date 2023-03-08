24c66fbc876fb3fa6e521b138cd20731b200e7bb
Dan Kimball <dankimball@Dans-MacBook-Pro.local>
2023-03-08 16:09:06 -0700

    redux server & channels store

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,121 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import './Navigation.css';
-import { getServerById, getAllServers, getUserServers, deleteServerById, editServerById } from '../../store/servers'
-import { getAllChannelsInServer, getChannelById } from '../../store/channels'
+import './servers.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell, faChevronDown } from "@fortawesome/free-solid-svg-icons"
 
-function Navigation({ isLoaded }) {
-	const dispatch = useDispatch()
+function Navigation({ isLoaded }){
 
 	const sessionUser = useSelector(state => state.session.user);
-	const serverGuy = useSelector(state => state.servers.serverById)
-	const [showNotifications, setShowNotifications] = useState(false)
-
-	const [name, setName] = useState("");
-	const [icon, setIcon] = useState("");
-	const [channels, setChannels] = useState([]);
-
-
-	useEffect(() => {
-		dispatch(getAllServers());
-		dispatch(getUserServers());
-		dispatch(getServerById(8));
-
-	}, [dispatch]);
-
-	useEffect(() => {
-		if (serverGuy) {
-			setName(serverGuy.name);
-			setIcon(serverGuy.icon);
-			dispatch(getAllChannelsInServer(serverGuy));
-		}
-	}, [serverGuy]);
-
-	useEffect(() => {
-		dispatch(getChannelById(serverGuy.id, 7));
-	  }, [dispatch, serverGuy.id]);
-	
-
+	const [showNotifications, setShowNotifications ] = useState(false)
 
 	const handleNotificationClick = () => {
 		setShowNotifications(!showNotifications);
 	}
 
-	//-------------
-
-
-
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-
-		const editedServer = {
-			id: serverGuy.id,
-			name,
-			icon,
-		};
-
-		dispatch(editServerById(editedServer));
-	};
-	const handleDelete = async (e) => {
-		e.preventDefault()
-		console.log("did this work?")
-		dispatch(deleteServerById(9))
-
-
-	}
-	//--------------------
-
 	return (
-		<>
-			<nav className='navbar'>
-				<div className='left-nav-bar'>
-					<div className='nav-logo'>
-						<img src="/discord_logo.svg" alt="Discord Logo" />
-					</div>
+	<>
+		<nav className='navbar'>
+			<div className='left-nav-bar'>
+				<div className='nav-logo'>
+					<img src="/discord_logo.svg" alt="Discord Logo"/>
 				</div>
+			</div>
 				<li>
-					<NavLink exact to="/">Home</NavLink>
+				<NavLink exact to="/">Home</NavLink>
 				</li>
 				{isLoaded && (
-					<li>
-						<ProfileButton user={sessionUser} />
-					</li>
+				<li>
+					<ProfileButton user={sessionUser} />
+				</li>
 				)}
 
-				<form className='create' onSubmit={handleSubmit}>
-
-					<label>Name:</label>
-					<input
-						type="text"
-						onChange={(e) => setName(e.target.value)}
-						value={name}
-					/>
-
-					<label>Icon: </label>
-					<input
-						type="text"
-						onChange={(e) => setIcon(e.target.value)}
-						value={icon}
-					/>
-					<button>edit Server</button>
-
-				</form>
-				<button onClick={handleDelete}>Delete server</button>
-
-
-
-
-			</nav>
-			<div>
-      {channels.map(channel => (
-        <div key={channel.id}>{channel.name}</div>
-      ))}
-    </div>
-		</>
+		</nav>
+	</>
 	);
 }
 
