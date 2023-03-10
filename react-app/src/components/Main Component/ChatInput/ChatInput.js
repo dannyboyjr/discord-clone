db9bdba24bea34969ee5ef68e96349ee857188e4
scottkonner <scott_konner@yahoo.com>
2023-03-09 20:09:21 -0500

    rolled back to previous commit "d44fb42ef30a7d1f942983afe88a2647577b3bef"

--- react-app/src/components/Main Component/ChatInput/ChatInput.js
+++ react-app/src/components/Main Component/ChatInput/ChatInput.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createMessageInChannel } from '../../../store/channels';
+import { useParams } from 'react-router-dom';
 import './ChatInput.css';
 
 function ChatInput() {
+  const {serverId, channelId } = useParams()
+  const [content, setContent] = useState("")
+  const dispatch = useDispatch();
+
+  
   return (
     <div className="chat-input-container">
       <input type="text" placeholder="Type a message..." />
