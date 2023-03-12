ee66cc30f328cbf4052080e6e8fb4417daeaffdb
scottkonner <scott_konner@yahoo.com>
2023-03-11 19:04:15 -0500

    still pulling over delete channel button, issue with dispatch and serverChannels

--- react-app/src/components/Main Component/Channels/ChannelsCard/ChannelsCard.js
+++ react-app/src/components/Main Component/Channels/ChannelsCard/ChannelsCard.js
@@ -7,6 +7,19 @@ import EditChannelModal  from '../../../EditChannelModal'
 
 const ChannelsCard = ({ channel, serverId }) => {
 
+    const channelsArr = Object.values(serverChannels)
+
+    const handleDelete = () => {
+        if (channelsArr.length > 1){
+        dispatch(deleteChannelById(channel.server_id, channel.id )).then(() => {
+        })}
+        else {
+            return window.alert('Cannot delete a channel from a server with only one channel')
+        }
+
+    };
+
+
 return (
     <div>
       {/* Conditionally render a NavLink or a regular link based on the private property */}
@@ -28,9 +41,13 @@ return (
                 buttonText="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Font_Awesome_5_regular_edit.svg/1200px-Font_Awesome_5_regular_edit.svg.png"
                 modalComponent={<EditChannelModal channel={channel} />}
                 />
-        </div>
+            </div>
+            <span className="delete-icon" onClick={handleDelete}>
+                <i className="fa fa-times"></i>
+            </span>
+
           </div>
-          
+
         </NavLink>
 
       )}
