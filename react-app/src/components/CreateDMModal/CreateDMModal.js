bd99248ec3bf74e91f5345975b87f1a08b109d7f
Dan Kimball <dankimball@Dans-MacBook-Pro.local>
2023-03-12 10:10:32 -0600

    pushing to dev

--- react-app/src/components/CreateDMModal/CreateDMModal.js
+++ react-app/src/components/CreateDMModal/CreateDMModal.js
@@ -15,8 +15,8 @@ const CreateDMModal = ({channels}) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    // Verify sorts through dms to check if an open dm channel exist. if it does exit user is rerouted to open dm
     const verify = channels.find(item => {
-      console.log("CONSTOLLOSDIFJDS")
       console.log(item.server.members[1].user.username == username)
       return username == item.server.members[0].user.username || username == item.server.members[1].user.username 
     })
