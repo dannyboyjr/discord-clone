cf5d37e9ff5266db11428f77ae9af02187cc3d16
Dan Kimball <dankimball@Dans-MacBook-Pro.local>
2023-03-13 13:54:46 -0600

    made splash page ugly but had to add contributors with zero time to style better

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -4,7 +4,7 @@ import { NavLink, useHistory } from "react-router-dom";
 import { getUserServers } from "../../store/servers";
 import "./SplashPage.css"
 import splashImage from "./discord-background.jpg"
-
+import GitHubImg from '../../assets/github.png'
 
 
 
@@ -30,10 +30,42 @@ function SplashPage({isLoaded}) {
                 <p className='splash-welcome-text'>
                     Welcome to Discord!  You can log in to your account from here. Don't have one? You can register below to sign up!
                 </p>
+                
                 <div  className='splash-container-links'>
                     <NavLink to='/login' className="login-button" activeClassName="active">Log In</NavLink>
                     <NavLink to='/signup' className="signup-button" activeClassName="active">Sign Up</NavLink>
+                   
+                    <a href="https://github.com/dannyboyjr">
+                        <img src={GitHubImg} alt="Dan imball" />
+                        <p>Dan Kimball</p>
+                        </a>
+                        <a href="https://github.com/scottkonner">
+                        <img src={GitHubImg} alt="Scott Konner" />
+                        <p>Scott Konner</p>
+                        </a>
+                        <a href="https://github.com/natie92">
+                        <img src={GitHubImg} alt="Natalia Miller" />
+                        <p>Natalia Miller</p>
+                        </a>
                 </div>
+                {/* <div>
+                    <h4>Contributors</h4>
+                    <div>
+                        <a href="https://github.com/dannyboyjr">
+                        <img src={GitHubImg} alt="Dan imball" />
+                        <p>Dan Kimball</p>
+                        </a>
+                        <a href="https://github.com/scottkonner">
+                        <img src={GitHubImg} alt="Scott Konner" />
+                        <p>Scott Konner</p>
+                        </a>
+                        <a href="https://github.com/natie92">
+                        <img src={GitHubImg} alt="Natalia Miller" />
+                        <p>Natalia Miller</p>
+                        </a>
+                    </div>
+                </div> */}
+        
             </div>
         </>
     )
