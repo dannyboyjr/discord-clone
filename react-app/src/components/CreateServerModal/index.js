2826c168558dc24e65effb7b6590f413a899041c
natie92 <83873748+natie92@users.noreply.github.com>
2023-03-13 13:49:35 -0400

    working on create server modal css

--- react-app/src/components/CreateServerModal/index.js
+++ react-app/src/components/CreateServerModal/index.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { createAServer } from '../../store/servers';
 import { useDispatch } from 'react-redux';
 import { useModal } from "../../context/Modal";
+// import "../../context/Modal.css"
+import "./CreateServerModal.css"
 
 
 const CreateServerModal = () => {
@@ -25,9 +27,11 @@ const CreateServerModal = () => {
   return (
     <div className="create-server-modal">
       <div className="create-server-modal-content">
-
         <h2>Create a Server</h2>
+        <p> Your server is where you and your friends hang out. Make yours and start talking!</p>
+
         <form onSubmit={handleSubmit}>
+
           <div className="input-container">
             <label htmlFor="name">Server Name</label>
             <input
@@ -37,8 +41,8 @@ const CreateServerModal = () => {
               onChange={(e) => setName(e.target.value)}
               required
             />
-
           </div>
+
           <div className="input-container">
             <label htmlFor="icon">Server Icon</label>
             <input
@@ -47,15 +51,17 @@ const CreateServerModal = () => {
               value={icon}
               onChange={(e) => setIcon(e.target.value)}
             />
-
           </div>
+
           <div className="button-container">
-            <button type="submit">Create</button>
-            <button type="button" onClick={closeModal}>
+            <button className="submit-server-modal-btn" type="submit">Create</button>
+            <button className='close-server-modal-btn' type="button" onClick={closeModal}>
               Close
             </button>
           </div>
+
         </form>
+
       </div>
     </div>
   );
