2e4448d23c46d49c183831cc11ac379500e660f2
scottkonner <scott_konner@yahoo.com>
2023-04-04 19:52:04 -0400

    change to server_members seeder to fix autoincrement error

--- app/seeds/server_members.py
+++ app/seeds/server_members.py
@@ -2,22 +2,18 @@ from ..models import db, Server_Member, environment, SCHEMA
 
 server_members = [
     {
-        "id": 1,
         "user_id": 1,
         "server_id": 1
     },
     {
-        "id": 2,
         "user_id": 2,
         "server_id": 2
     },
     {
-        "id": 3,
         "user_id": 3,
         "server_id": 3,
     },
     {
-        "id": 4,
         "user_id": 1,
         "server_id": 3
     },
